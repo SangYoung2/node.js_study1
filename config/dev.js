3b9ace8a5dadd592049e4e2422ea8e7f7f8a60e7
SangYoung2 <tmskan8@naver.com>
2023-11-18 23:05:51 +0900

    23/11/18 Node.js Study Day2 - Node.Js 설정 및 MongoDB 세팅

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://tmskan8:rlatkddud8@boilerplate.sdn0sa9.mongodb.net/?retryWrites=true&w=majority'
+}
--- model/user.js
+++ model/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -28,6 +30,23 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number
+    }
+})
+
+userSchema.pre('save', function (next){ // save를 실행하기 전에 거쳐가게 만듬
+    // 비밀번호를 암호화 시킨다.
+    var user = this; // 현재 받은 정보가 입력된 user
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function (err, salt){ // salt는 암호화 하는 작업
+            if(err) return next(err)
+            // 비밀번호의 데이터 값을 변환
+            bcrypt.hash(user.password, salt, function (err, hash) { // hash는 암호화된 비밀번호
+                if(err) return next(err)
+                // 암호화 된 비밀번호가 생성되었다면 user의 password값을 hash값으로 바꿔준다.
+                user.password = hash;
+                next();
+            })
+        })
     }
 })
 
